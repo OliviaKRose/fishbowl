c47f822c0d0f973ca0eed80b55bb0564c1f07878
Olivia Rose <olivia.rose@softwire.com>
2023-05-03 16:11:26 +0100

    GIF?

--- giphy.js
+++ giphy.js
@@ -1,6 +1,6 @@
 const apiKey = "VnIf8OBf4s5wH4F2jhlXbulOmjEitmxp";
 const myButton = document.getElementById('myButton');
-myButton.addEventListener('click', function () {
+myButton.addEventListener('click', function (event) {
     console.log("click");
     // fetch(`api.giphy.com/v1/gifs/random?tag=fish&api_key=${apiKey}&rating=g`).then(function (response) {
     //     const gif = response.data;
